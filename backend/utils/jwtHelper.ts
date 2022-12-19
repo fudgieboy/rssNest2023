9cb4543ec83ffd2ff1083808e9bb2714b6a203e9
Jude <luddensj@gmail.com>
2022-12-19 18:59:12 -0500

    reincluded some files

--- backend/utils/jwtHelper.ts
+++ backend/utils/jwtHelper.ts
@@ -1,31 +1,31 @@
-// import * as AES from "gibberish-aes/src/gibberish-aes.js";
-// import * as jwt from "jsonwebtoken";
-// // import {config} from "../../localconfig";
-// import {config} from "../../apiKeys";
+import * as AES from "gibberish-aes/src/gibberish-aes.js";
+import * as jwt from "jsonwebtoken";
+// import {config} from "../../localconfig";
+import {config} from "../../apiKeys";
 
-// const jwtSecret = config.user.secret;
-// const aesSecret = config.aes.secret;
-// const aesPass = config.aes.pass;
+const jwtSecret = config.user.secret;
+const aesSecret = config.aes.secret;
+const aesPass = config.aes.pass;
 
-// const verifyLoginToken = (encodedData) => {
-//   return AES.dec(jwt.verify(encodedData, jwtSecret).data, aesPass);
-// };
+const verifyLoginToken = (encodedData) => {
+  return AES.dec(jwt.verify(encodedData, jwtSecret).data, aesPass);
+};
 
-// const getLoginToken = (data) => {
-//   let encoded = AES.enc(data, aesPass);
+const getLoginToken = (data) => {
+  let encoded = AES.enc(data, aesPass);
 
-//   return jwt.sign(
-//     { data: encoded},
-//     jwtSecret,
-//     { expiresIn: "1h" }
-//   );
-// };
-// // export class jwtHelper {
-// //   getLoginToken
-// //   verifyLoginToken
-// // };
-
-// export {
-//   getLoginToken,
+  return jwt.sign(
+    { data: encoded},
+    jwtSecret,
+    { expiresIn: "1h" }
+  );
+};
+// export class jwtHelper {
+//   getLoginToken
 //   verifyLoginToken
 // };
+
+export {
+  getLoginToken,
+  verifyLoginToken
+};
